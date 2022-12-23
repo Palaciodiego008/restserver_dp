8a5a3bfc2adc1db6484660e0d728b0b1a1deb224
Diego Palacio <palaciodiego099@gmail.com>
2022-12-23 11:50:16 -0500

    adding script to start app

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ import userRoutes from '../routes/user.js';
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
 
 
         // Middlewares
