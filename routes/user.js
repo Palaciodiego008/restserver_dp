2059f0372c4c9a55f8de10ae336404bbc811ac88
Diego Palacio <palaciodiego099@gmail.com>
2023-03-21 17:31:03 -0500

    Formating code with prettier

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,26 @@
-import {Router} from 'express';
-import {deleteUsers, getUsers, postUsers, putUsers} from '../controllers/users.js';
-import {check} from 'express-validator';
-const router = Router();
-
-
+import { Router } from "express";
+import {
+  deleteUsers,
+  getUsers,
+  postUsers,
+  putUsers,
+} from "../controllers/users.js";
+import { check } from "express-validator";
 
 let validations = [
-    check('email', 'Email is not valid.').isEmail(),
-]
+  check("email", "Email is not valid.").isEmail(),
+  check("name", "Name is required.").not().isEmpty(),
+  check("password", "Password must be at least 6 characters.").isLength({
+    min: 6,
+  }),
+  check("role", "Role is not valid.").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+];
 
-router.get('/', getUsers)
-router.put('/:id', putUsers)
-router.post('/', validations ,postUsers)
-router.delete('/', deleteUsers)
+const router = Router();
 
+router.get("/", getUsers);
+router.put("/:id", putUsers);
+router.post("/", validations, postUsers);
+router.delete("/", deleteUsers);
 
 export default router;
